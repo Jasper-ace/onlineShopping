385ffe03dbefd8c3215806cb979846668dcc9371
Jasper-ace <130119059+Jasper-ace@users.noreply.github.com>
2024-11-29 15:58:38 +0800

    Delete java directory

--- java/TermGrade.java
+++
@@ -1,9 +0,0 @@
-public class TermGrade {
-    double dPrelim, dMidterm, dFinals;
-
-    public TermGrade(double dPrelim, double dMidterm, double dFinals) {
-        this.dPrelim = dPrelim;
-        this.dMidterm = dMidterm;
-        this.dFinals = dFinals;
-    }
-}
